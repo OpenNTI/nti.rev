b9348ffac69fdfcc3f919b3bb3158838336e7a50
Ryan Browning <ryan.browning@nextthought.com>
2017-10-06 11:23:29 -0500

    Added __init__.py file in tests

---
+++ src/nti/rev/tests/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from zope.component.testlayer import ZCMLFileLayer
+
+import nti.rev
+
+NTIRevLayer = ZCMLFileLayer(nti.rev, 'configure.zcml')
