36cbe1ba16ff443b4011386af9a64d7cee0c44be
Ryan Browning <ryan.browning@nextthought.com>
2017-10-06 10:51:07 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,60 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.rev',
+    version=_read('version.txt').strip(),
+    author='Ryan Browning',
+    author_email='ryan.browning@nextthought.com',
+    description="NTI Rev",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='Rev',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.rev",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'six',
+        'zope.component',
+        'zope.deferredimport',
+        'zope.deprecation',
+        'zope.interface',
+        'zope.schema'
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
+)
