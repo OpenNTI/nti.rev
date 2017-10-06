610823ce7b982c46793b0a1ea445cc5869ec8380
Carlos Sanchez <papachoco@gmail.com>
2017-10-06 16:53:13 -0500

    add message factory

--- src/nti/rev/__init__.py
+++ src/nti/rev/__init__.py
@@ -7,3 +7,6 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
+
+import zope.i18nmessageid
+MessageFactory = zope.i18nmessageid.MessageFactory('nti.rev')
