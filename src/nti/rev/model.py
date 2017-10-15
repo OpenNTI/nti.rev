de046618ee205f792003f4f1c9d9ff545b074855
Ryan Browning <ryan.browning@nextthought.com>
2017-10-14 20:41:43 -0500

    add model file

---
+++ src/nti/rev/model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from .interfaces import IClientAPIKey
+from .interfaces import IUserAPIKey
+from .interfaces import ICredentials
+from .interfaces import ISourceFileUpload
+from .interfaces import IInput
+from .interfaces import ISourceFileInput
+from .interfaces import IExternalLinkInput
+from .interfaces import ISpeaker
+from .interfaces import ISpeakers
+from .interfaces import IMonologueElement
+from .interfaces import IMonologueElements
+from .interfaces import IMonologue
+from .interfaces import IMonologues
+from .interfaces import IComment
+from .interfaces import INotification
+from .interfaces import ILink
+from .interfaces import ILinks
+from .interfaces import IAttachment
+from .interfaces import IAttachments
+from .interfaces import IAttachmentContent
+from .interfaces import IOrderRequest
+from .interfaces import IOrderDetails
+from .interfaces import IOrder
+from .interfaces import IOrders
+from .interfaces import ITranscriptionOptions
+from .interfaces import ITranscriptionOrderRequest
+from .interfaces import ICaption
+from .interfaces import ICaptionOrderDetails
+from .interfaces import ICaptionOptions
+from .interfaces import ICaptionOrderRequest
+from .interfaces import ICaptionOrder
+from .interfaces import ITranscription
+from .interfaces import ITranscriptionOrderDetails
+from .interfaces import ITranscriptionOrder
+
+
