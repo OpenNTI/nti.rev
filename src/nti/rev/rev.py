bab6424e8f230eb1de2ad4423cbc6842bcd8f661
Ryan Browning <ryan.browning@nextthought.com>
2017-11-08 23:06:25 -0600

    begin implementing get_orders method

--- src/nti/rev/rev.py
+++ src/nti/rev/rev.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import urlparse
+
 from zope import component
 from zope import interface
 
@@ -32,6 +34,47 @@ class RevClient(SchemaConfigured):
     def __init__(self, BaseURL=None, credentials=None):
         self.BaseURL = BaseURL
         self.session = make_session(self.BaseURL, credentials=credentials)
+    
+    def url_for_operation(self, entity_type, params={}):
+        """
+        Constructs a URL for the desired entity and params
+        """
+
+        base = self.BaseURL 
+        # append the operation-specific component to the base URL
+        joined = urlparse.urljoin(base, entity_type)
+        
+        # parse URL into 6 components: scheme, netloc, path, params, query, fragment
+        components = list(urlparse.urlparse(joined))
+        
+        # create dictionary by parsing query string (components[4]) into name, value pairs
+        query = dict(urlparse.parse_qsl(components[4]))
+        # add the params dictionary into the query dictionary
+        query.update(params)
+        
+        # convert query dictionary to percent-encoded string of
+        # key=value pairs separated by '&' characters
+        components[4] = urlencode(query)
+        
+        # concatenate URL components back into URL
+        return urlparse.urlunparse(components)
+    
+    def _orders_url(self, params):
+        return self.url_for_operation('orders', params=params)
+
+    def get_orders(self, pagenum=1, pageSize=25, orderNumber=None, referenceId=None):
+        url = self._orders_url(params={'pagenum': pagenum,
+                                       'pageSize': pageSize,
+                                       'orderNumber': orderNumber,
+                                       'referenceId': referenceId})
+        
+        # logger.info('Getting orders for FIXME')
+        logger.debug('Using Rev API %s', url)
+        
+        # try:
+        #    response = self.session.get(url)
+            
+        # TODO: finish method
 
 #     def upload_source_file(self, source_file_upload):
         
